8893f781e7d023e900acdce1f8db8b03741198de
sumit2719 <83194837+sumit2719@users.noreply.github.com>
2024-06-30 18:14:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 // middleware
 app.use(cors(
 	{
-		origin: ["https://quadb-client.vercel.app"],
+		origin: ["http://localhost:5173/"],
 		methods: ["GET", "POST"],
 		credentials: true
 
