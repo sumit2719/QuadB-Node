b21879b53300fc72b5a0e400bfd4201786572bdc
sumit2719 <sumitsingh1339@gmail.com>
2024-06-30 18:03:59 +0530

    vercel

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 // middleware
 app.use(cors(
 	{
-		origin: ["https://quadb-node-7m77.onrender.com/"],
+		origin: ["https://quadb-client.vercel.app"],
 		methods: ["GET", "POST"],
 		credentials: true
 
